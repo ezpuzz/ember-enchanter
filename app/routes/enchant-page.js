cc929e016fc177ff9477d654baa4d67d820a6ab8
Emory Petermann <emory@onlyfor.us>
2015-08-26 22:12:03 -0700

    more sophisticated dummy application

---
+++ app/routes/enchant-page.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-enchanter/routes/enchant-page';
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,11 +1,13 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-  location: config.locationType
+const Router = Ember.Router.extend({
+  location: config.locationType,
 });
 
 Router.map(function() {
+  this.route('first-wizard', { path: '/first-wizard/:page_slug' });
+  this.route('second-wizard', { path: '/second-wizard/:page_slug' });
 });
 
 export default Router;
